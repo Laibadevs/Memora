c3dbf5708e92b7505344c403de5782b17ee3103c
Laibadevs <laibalrb68@gmail.com>
2026-07-30 21:43:22 +0500

    login page is add

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
           on Home via its hash (e.g. /#features). Add more top-level
           routes here later (e.g. /pricing, /docs) as real pages exist. */}
       <Route path="/" element={<Home />} />
-
+      <Route path="/login" element={<Login />} />
     </Routes>
   )
 }
