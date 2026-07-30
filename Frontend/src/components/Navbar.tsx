cb206cc880d5d21a8e55f3cfa84a8e19a17f9163
Laibadevs <laibalrb68@gmail.com>
2026-07-30 21:41:18 +0500

    add login page

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { motion, AnimatePresence } from 'framer-motion'
 import { Menu, X } from 'lucide-react'
-import MemoraLogo from "../assets/Memora_Logo.png";
+import MemoraLogo from "../assets/LOGO.png";
 import MemoraWord from "../assets/memora_word.png";
+
 
 const NAV_LINKS = [
     { label: 'Features', id: 'features' },
@@ -25,6 +26,7 @@ export default function Navbar() {
     const [scrolled, setScrolled] = useState(false)
     const navigate = useNavigate()
     const location = useLocation()
+
 
     useEffect(() => {
         const onScroll = () => setScrolled(window.scrollY > 12)
@@ -68,7 +70,7 @@ export default function Navbar() {
                     className="flex items-center gap-4 shrink-0"
                     aria-label="Memora home"
                 >
-                    <img src={MemoraLogo} alt="" className="h-5 w-auto  md:h-10" />
+                    <img src={MemoraLogo} alt="" className="h-20 w-auto  md:h-15" />
                     <img src={MemoraWord} alt="Memora" className="h-4 w-auto z-10 md:h-[20px]" />
                 </motion.button>
 
@@ -96,6 +98,7 @@ export default function Navbar() {
                     className="hidden md:flex items-center gap-6"
                 >
                     <button
+                        onClick={() => navigate("/login")}
                         className="group relative px-3 py-2 text-base font-medium text-white/80 hover:text-white transition-all duration-300"
                     >
                         Log in
