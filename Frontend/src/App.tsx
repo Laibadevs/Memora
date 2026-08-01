672329e0eceafe8d9fceac91959e4e612512ca38
Laibadevs <laibalrb68@gmail.com>
2026-08-01 22:48:39 +0500

    add routes of signup,forgetpassword,resestpassword,passwordchanges,emailsent

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from "./pages/Login";
-
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgetPassowrd";
+import ResetPassword from "./pages/ResetPassword";
+import PasswordChanges from "./pages/PasswordChanges";
+import EmailSent from "./pages/EmailSent";
 function App() {
   return (
     <Routes>
@@ -10,6 +14,11 @@ function App() {
           routes here later (e.g. /pricing, /docs) as real pages exist. */}
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/password-changes" element={<PasswordChanges />} />
+      <Route path="/email-sent" element={<EmailSent />} />
     </Routes>
   )
 }
