5d79205bfbf5d09fda4da61abd1f827f05c979ca
Laibadevs <laibalrb68@gmail.com>
2026-08-03 22:31:11 +0500

    pages

---
+++ Frontend/src/pages/ForgetPassowrd.tsx
@@ -0,0 +1,10 @@
+import PlainAuthLayout from "../layout/PlainAuthLayout";
+import ForgotPasswordForm from "../components/auth/ForgetPasswordForm";
+
+export default function ForgetPasswordPage() {
+    return (
+        <PlainAuthLayout>
+            <ForgotPasswordForm />
+        </PlainAuthLayout>
+    );
+}
