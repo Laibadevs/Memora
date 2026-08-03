7f32c004bd89c6cb4487a5fab90db853e7491efe
Laibadevs <laibalrb68@gmail.com>
2026-08-03 22:28:39 +0500

    responsive

---
+++ Frontend/src/pages/Signup.tsx
@@ -0,0 +1,10 @@
+import SignupLayout from "../layout/SignupLayout";
+import SignupForm from "../components/auth/SignupForm";
+
+export default function SignupPage() {
+    return (
+        <SignupLayout>
+            <SignupForm />
+        </SignupLayout>
+    );
+}
