a08533837e71fe35dc591658f4e1b4cd7de94279
Laibadevs <laibalrb68@gmail.com>
2026-08-03 22:11:11 +0500

    email sent after forgot password

---
+++ Frontend/src/pages/EmailSent.tsx
@@ -0,0 +1,10 @@
+import PlainAuthLayout from "../layout/PlainAuthLayout";
+import EmailSentCard from "../components/auth/EmailSentCard";
+
+export default function EmailSentPage() {
+    return (
+        <PlainAuthLayout>
+            <EmailSentCard />
+        </PlainAuthLayout>
+    );
+}
